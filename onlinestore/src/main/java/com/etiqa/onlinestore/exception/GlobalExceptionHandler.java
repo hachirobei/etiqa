2131b3084c5c45bce68cb7dfd019d858dc72a2a4
Muhammad Hafizoddin Bin Roslan <muhammadhafizoddinbinroslan@ITD00MB05.local>
2024-04-27 03:27:48 +0800

    1.3
    add logger and postman collection

--- onlinestore/src/main/java/com/etiqa/onlinestore/exception/GlobalExceptionHandler.java
+++ onlinestore/src/main/java/com/etiqa/onlinestore/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.etiqa.onlinestore.exception;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,15 +17,19 @@ import com.etiqa.onlinestore.dto.ApiResponse;
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+	private static final Logger logger = Logger.getLogger(GlobalExceptionHandler.class.getName());
+	
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<String>> handleAllExceptions(Exception ex) {
+        logger.log(Level.SEVERE, "An exception occurred: " + ex.getMessage(), ex);
         return new ResponseEntity<>(ApiResponse.failure("500", ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
     
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-        return new ResponseEntity<>(ApiResponse.failure("404", ex.getMessage()),HttpStatus.NOT_FOUND);
+        logger.log(Level.WARNING, "Resource not found: " + ex.getMessage(), ex);
+        return new ResponseEntity<>(ApiResponse.failure("404", ex.getMessage()), HttpStatus.NOT_FOUND);
     }
 }
 
